7ea60b89899965a7cae937dc6b7e2c89b34ecc6b
Nathan Florendo <nathan.florendo1@gmail.com>
2025-06-14 22:23:10 -0500

    added tabs and inialized heatmap calendar with dummy data

---
+++ MemoryVault-client/src/components/Tabs/Tabs.tsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import './Tabs.css';
+import NoteList from '../NoteList/NoteList';
+import Calendar from '../Calendar/Calendar';
+import type { Note } from '../types';
+
+interface TabsProp {
+    state: Note[];
+    deleteNote: (id: string) => void;
+    onSelectedNote: (note: Note | null) => void;
+}
+
+export default function Tabs({state, deleteNote, onSelectedNote}: TabsProp) {
+    const [activeTab, setActiveTab] = useState("Recent");
+
+    const tabs = [
+        {
+            name: 'Recent',
+            content: (
+                <NoteList
+                    state={state}
+                    deleteNote={deleteNote}
+                    onSelectedNote={onSelectedNote}
+                />
+            ),
+        },
+        {
+            name: 'Calendar',
+            content: (
+                <div className="calendar-wrapper">
+                    <Calendar/>
+                </div>
+            ),
+        }
+    ]
+
+    return (
+        <>
+            <div className="tab-titles">
+                {tabs.map((tab) => (
+                    <div
+                        key={tab.name}
+                        className={`tab-links ${activeTab === tab.name ? 'active-link' : ''}`}
+                        onClick={() => setActiveTab(tab.name)}
+                    >
+                        {tab.name}
+                    </div>
+                ))}
+            </div>
+
+            {tabs.map((tab) => (
+                <div
+                    key={tab.name}
+                    className={`tab-contents ${activeTab === tab.name ? 'active-tab' : ''}`}
+                >
+                    {activeTab === tab.name && tab.content}
+                </div>
+            ))}
+        </>
+    );
+}
